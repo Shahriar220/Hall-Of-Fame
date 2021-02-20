7a7def6202dbd119f6c75d6f2dbfd0e727fdab69
Shahriar220 <shahriar.uchchash@gmail.com>
2021-02-20 12:15:28 +0600

    youtube hall of fame creation

---
+++ halls/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Hall,Video
+from django.contrib.auth import authenticate,login
+from .forms import VideoForm,SearchForm
+from django.http import Http404
+import urllib
+import requests
+from django.forms.utils import ErrorList
+
+YOUTUBE_API_KEY='AIzaSyBy4_OLLkPhEwBzmYXPHRna7x5MbqA29vo'
+
+def home(request):
+    return render(request,'halls/home.html',{})
+
+def add_video(request,pk):
+
+    form=VideoForm()
+    
+    search_form=SearchForm()
+    hall=Hall.objects.get(pk=pk)
+    if not hall.user == request.user:
+        raise Http404
+
+    if request.method =='POST':
+        form=VideoForm(request.POST)
+        if form.is_valid():
+            video=Video()
+            video.hall=hall
+            video.url=form.cleaned_data['url']
+            parsed_url=urllib.parse.urlparse(video.url)
+            video_id=urllib.parse.parse_qs(parsed_url.query).get('v')
+
+            if video_id:
+                video.youtube_id=video_id[0]
+                response=requests.get(f'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={video_id[0]}&key={YOUTUBE_API_KEY}') 
+                json=response.json()
+                title=json['items'][0]['snippet']['title']
+                print(title)
+                video.title=title
+                video.save()
+                return redirect('detail_hall',pk)
+            else:
+                errors=form._errors.setdefault('url',ErrorList())
+                errors.append('Needs to be a Youtube url')
+
+    return render(request,'halls/add_video.html',{'form':form,'search_form':search_form,'hall':hall})
+
+def dashboard(request):
+    return render(request,'halls/dashboard.html',{})
+
+class SignUp(generic.CreateView):
+    form_class=UserCreationForm
+    success_url=reverse_lazy('home')
+    template_name='registration/signup.html'
+
+    def form_valid(self,form):
+        view=super(SignUp,self).form_valid(form)
+        username,password=form.cleaned_data.get('username'),form.cleaned_data.get('password1')
+        user=authenticate(username=username,password=password)
+        login(self.request,user)
+        return view
+
+class CreateHall(generic.CreateView):
+    model=Hall
+    fields=['title']
+    template_name='halls/create_hall.html'
+    success_url=reverse_lazy('dashboard')
+
+    def form_valid(self,form):
+        form.instance.user=self.request.user
+        super(CreateHall,self).form_valid(form)
+        return redirect('home')
+
+class DetailHall(generic.DetailView):
+    model=Hall
+    template_name='halls/detail_hall.html'
+
+
+class UpdateHall(generic.UpdateView):
+    model=Hall
+    template_name='halls/update_hall.html'
+    fields=['title']
+    success_url=reverse_lazy('dashboard')
+
+class DeleteHall(generic.DeleteView):
+    model=Hall
+    template_name='halls/delete_hall.html'
+    success_url=reverse_lazy('dashboard')
---
+++ hofvidz/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin,auth
+from django.urls import path
+from halls import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('admin/', admin.site.urls),
+    path('dashboard',views.dashboard,name='dashboard'),
+    #auth
+    path('signup',views.SignUp.as_view(),name='signup'),
+    path('login',auth.views.LoginView.as_view(),name='login'),
+    path('logout',auth.views.LogoutView.as_view(),name='logout'),
+
+    #hall
+    path('halloffame/create',views.CreateHall.as_view(),name='create_hall'),
+    path('halloffame/<int:pk>',views.DetailHall.as_view(),name='detail_hall'),
+    path('halloffame/<int:pk>/update',views.UpdateHall.as_view(),name='update_hall'),
+    path('halloffame/<int:pk>/delete',views.DeleteHall.as_view(),name='delete_hall'),
+
+    #video
+    path('halloffame/<int:pk>/addvideo',views.add_video,name='add_video'),
+]
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
