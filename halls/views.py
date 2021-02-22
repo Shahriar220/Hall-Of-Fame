f9d1394ca101cf13e918f8cd9713ff6d637be504
Shahriar220 <shahriar.uchchash@gmail.com>
2021-02-22 12:16:13 +0600

    all the files included

--- halls/views.py
+++ halls/views.py
@@ -1,3 +1,4 @@
+'''AIzaSyAiOVcZq9Tle7CwTg7bd3Nj8ouAeMzvQUw'''
 from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views import generic
@@ -5,16 +6,21 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Hall,Video
 from django.contrib.auth import authenticate,login
 from .forms import VideoForm,SearchForm
-from django.http import Http404
+from django.http import Http404,JsonResponse
 import urllib
 import requests
 from django.forms.utils import ErrorList
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-YOUTUBE_API_KEY='AIzaSyBy4_OLLkPhEwBzmYXPHRna7x5MbqA29vo'
+YOUTUBE_API_KEY=''
 
 def home(request):
-    return render(request,'halls/home.html',{})
+    recent_halls=Hall.objects.all().order_by('-id')[:3]
+    popular_halls=[]
+    return render(request,'halls/home.html',{'recent_halls':recent_halls,'popular_halls':popular_halls})
 
+@login_required
 def add_video(request,pk):
 
     form=VideoForm()
@@ -48,12 +54,36 @@ def add_video(request,pk):
 
     return render(request,'halls/add_video.html',{'form':form,'search_form':search_form,'hall':hall})
 
+@login_required
 def dashboard(request):
-    return render(request,'halls/dashboard.html',{})
+    halls=Hall.objects.filter(user=request.user)
+
+    return render(request,'halls/dashboard.html',{'halls':halls})
+
+@login_required
+def video_search(request):
+    search_form = SearchForm(request.GET)
+    if search_form.is_valid():
+        encoded_search_term = urllib.parse.quote(search_form.cleaned_data['search_term'])
+        response = requests.get(f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q={ encoded_search_term }&key={ YOUTUBE_API_KEY }')
+        return JsonResponse(response.json())
+    return JsonResponse({'error':'Not able to validate form'})
+
+
+class DeleteVideo(LoginRequiredMixin,generic.DeleteView):
+    model=Video
+    template_name='halls/delete_video.html'
+    success_url=reverse_lazy('dashboard')
+
+    def get_object(self):
+        video=super(DeleteVideo,self).get_object()
+        if not video.hall.user == self.request.user:
+            raise Http404
+        return video
 
 class SignUp(generic.CreateView):
     form_class=UserCreationForm
-    success_url=reverse_lazy('home')
+    success_url=reverse_lazy('dashboard')
     template_name='registration/signup.html'
 
     def form_valid(self,form):
@@ -63,7 +93,7 @@ class SignUp(generic.CreateView):
         login(self.request,user)
         return view
 
-class CreateHall(generic.CreateView):
+class CreateHall(LoginRequiredMixin,generic.CreateView):
     model=Hall
     fields=['title']
     template_name='halls/create_hall.html'
@@ -72,20 +102,31 @@ class CreateHall(generic.CreateView):
     def form_valid(self,form):
         form.instance.user=self.request.user
         super(CreateHall,self).form_valid(form)
-        return redirect('home')
+        return redirect('dashboard')
 
-class DetailHall(generic.DetailView):
+class DetailHall(LoginRequiredMixin,generic.DetailView):
     model=Hall
     template_name='halls/detail_hall.html'
 
 
-class UpdateHall(generic.UpdateView):
+class UpdateHall(LoginRequiredMixin,generic.UpdateView):
     model=Hall
     template_name='halls/update_hall.html'
     fields=['title']
     success_url=reverse_lazy('dashboard')
 
-class DeleteHall(generic.DeleteView):
+    def get_object(self):
+        hall=super(UpdateHall,self).get_object()
+        if not hall.user == self.request.user:
+            raise Http404
+        return hall
+
+class DeleteHall(LoginRequiredMixin,generic.DeleteView):
     model=Hall
     template_name='halls/delete_hall.html'
     success_url=reverse_lazy('dashboard')
+    def get_object(self):
+        hall=super(DeleteHall,self).get_object()
+        if not hall.user == self.request.user:
+            raise Http404
+        return hall
